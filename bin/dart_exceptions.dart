7a96f49b10f7757158db2088e31275d73ff33b95
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-10-30 12:55:31 -0300

    .

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -18,7 +18,7 @@ void main() {
 
   // Fazendo transferência
   bool result = bankController.makeTransfer(
-      idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+      idSender: "Kako", idReceiver: "Ricarth", amount: 300);
 
   // Observando resultado
   print(result);
